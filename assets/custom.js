5b1224a4ac2130903131836f805fd7750425fcbe
Shoaib Ahmad <shoaib.ahmad@shopdev.co>
2020-09-17 21:35:19 +0500

    feat: sideNav links show active state once visited

--- assets/custom.js
+++ assets/custom.js
@@ -34,175 +34,210 @@
  * }));
  */
 
-if(window.location.pathname.indexOf('faqs-category') !== -1) {
-    executeFaqsScript();
+$(document).ready(() => {
+  activeNavList();
+});
+
+if (window.location.pathname.indexOf("faqs-category") !== -1) {
+  executeFaqsScript();
 }
 
-// if(window.location.pathname.indexOf('/account/addresses') !== -1) {
-//     debugger
-// }
+/**
+ * Active nav tab from nav list once the user is logged-in and viewing one of the nav item i.e. My Addresses, My Loyalty Cards, My Orders etc.
+ */
+function activeNavList() {
+  let currentPathName = window.location.pathname;
+  const navList = $(".card__linklist-item");
+  navList.removeClass("text--strong");
+  let foundNavAnchor;
+  for (let i = 0; i < navList.length; i++) {
+    let item = navList[i];
+    if ($(item).attr("href") === currentPathName) {
+      foundNavAnchor = item;
+      break;
+    }
+  }
+  if (foundNavAnchor) {
+    $(foundNavAnchor).addClass("text--strong");
+  }
+}
 
 /* FAQs related custom script */
 function executeFaqsScript() {
-    $('.tab_contents:not(:first)').hide();
-    $("#category-title").html($('#tabs_container .tab:first').html());
-    
-    let tabNodes = document.querySelectorAll('.tab');
-    let pageUrl = window.location.pathname;
-    let searchParams = new URLSearchParams(window.location.search);
-    let categoryId = searchParams.get('category'); // route query param category id
+  $(".tab_contents:not(:first)").hide();
+  $("#category-title").html($("#tabs_container .tab:first").html());
 
-    /* Hack: if FAQs is visited through url /pages/click-and-collect then add query param (i.e.category) of 'Click and Collect' explicitly */
-    if(document.referrer.indexOf('collect') !== -1) {
-        searchParams.set('category', 5);
-        categoryId = searchParams.get('category');
-    }
+  let tabNodes = document.querySelectorAll(".tab");
+  let pageUrl = window.location.pathname;
+  let searchParams = new URLSearchParams(window.location.search);
+  let categoryId = searchParams.get("category"); // route query param category id
 
-    /* Hack: active first tab by default if no query param exists */
-    if(!categoryId) {
-        $('#tabs_container .tab:first').addClass('active');
-    }
+  /* Hack: if FAQs is visited through url /pages/click-and-collect then add query param (i.e.category) of 'Click and Collect' explicitly */
+  if (document.referrer.indexOf("collect") !== -1) {
+    searchParams.set("category", 5);
+    categoryId = searchParams.get("category");
+  }
 
-    for(let i=0; i<tabNodes.length; i++) {
-        let item = tabNodes[i];
-        let itemCategory = item.getAttribute('data-category');
-        if(itemCategory ===  categoryId) {
-            if(!item.classList.contains('active')) {
-                item.classList.add('active')
-            }
-            setTimeout(()=> { $(item).click() }, 10);
-            break;
-        }
+  /* Hack: active first tab by default if no query param exists */
+  if (!categoryId) {
+    $("#tabs_container .tab:first").addClass("active");
+  }
+
+  for (let i = 0; i < tabNodes.length; i++) {
+    let item = tabNodes[i];
+    let itemCategory = item.getAttribute("data-category");
+    if (itemCategory === categoryId) {
+      if (!item.classList.contains("active")) {
+        item.classList.add("active");
+      }
+      setTimeout(() => {
+        $(item).click();
+      }, 10);
+      break;
     }
+  }
 }
 
 /* Show respective category articles based on tab/category-title click */
-$('#tabs_container .tab').click(function() {
+$("#tabs_container .tab").click(function () {
+  /* Disable clicking the same tab more than once */
+  $("#tabs_container > .tabs > li > .tab:not(.active)").removeAttr("disabled");
+  if ($(this).attr("disabled") === "disabled") {
+    return;
+  }
 
-    /* Disable clicking the same tab more than once */
-    $('#tabs_container > .tabs > li > .tab:not(.active)').removeAttr("disabled");
-    if($(this).attr("disabled") === "disabled") {
-        return;
-    }
+  var target = $(this.rel);
+  $("#category-title").html(this.innerText);
+  $(".tab_contents").not(target).hide();
+  target.toggle();
+  $("#tabs_container > .tabs > li > a.active").removeClass("active");
+  $(this).addClass("active");
+  $(
+    "#tabs_container > .tab_contents_container > div.tab_contents_active"
+  ).removeClass("tab_contents_active");
+  $(this.rel).addClass("tab_contents_active");
 
-    var target = $(this.rel);
-    $("#category-title").html(this.innerText);
-    $('.tab_contents').not(target).hide();
-    target.toggle();
-    $('#tabs_container > .tabs > li > a.active').removeClass('active');
-    $(this).addClass('active');
-    $('#tabs_container > .tab_contents_container > div.tab_contents_active').removeClass('tab_contents_active');
-    $(this.rel).addClass('tab_contents_active');
-      
-    $(this).attr("disabled", "disabled");
-
+  $(this).attr("disabled", "disabled");
 });
 
 /* Redirect Url for Login/Signup. Method being called from header.liquid */
 function redirectMethod() {
-    console.log('applying redirect')
-    let redirectUrl = 'https://www-develop.smretailonline.com/login/?redirectUrl=https%3A%2F%2Fapi-develop.smretailonline.com/auth/shopify/sso';
-    window.location.href = redirectUrl;
+  console.log("applying redirect");
+  let redirectUrl =
+    "https://www-develop.smretailonline.com/login/?redirectUrl=https%3A%2F%2Fapi-develop.smretailonline.com/auth/shopify/sso";
+  window.location.href = redirectUrl;
 }
 
 /* Method being called from page-faq-static-template.liquid */
 function goToFaqsCategoryUrl() {
-    window.location.pathname = '/pages/faqs-category';
+  window.location.pathname = "/pages/faqs-category";
 }
 
 /* Reset new address form in addresses.liquid */
 function resetAdderessForm() {
-    document.getElementById('address_form_new').reset();
+  document.getElementById("address_form_new").reset();
 }
 
 /* Phone format in addresses.liquid */
-$('.phone-input').keydown(function(e){
-    let oldvalue = $(this).val();
-    let key = e.charCode || e.keyCode || 0;
-    if(key != 8) {
-        if($(this).val().length === 3) {
-            $(this).val($(this).val() + ' ');
-        }
-        if($(this).val().length === 7 || $(this).val().length === 11) {
-            $(this).val($(this).val() + '-');
-        }
+$(".phone-input").keydown(function (e) {
+  let oldvalue = $(this).val();
+  let key = e.charCode || e.keyCode || 0;
+  if (key != 8) {
+    if ($(this).val().length === 3) {
+      $(this).val($(this).val() + " ");
     }
-    setTimeout( () => {
-        if(this.value.indexOf('+63') !== 0) {
-            $(this).val(oldvalue);
-        } 
-    }, 1);
+    if ($(this).val().length === 7 || $(this).val().length === 11) {
+      $(this).val($(this).val() + "-");
+    }
+  }
+  setTimeout(() => {
+    if (this.value.indexOf("+63") !== 0) {
+      $(this).val(oldvalue);
+    }
+  }, 1);
 });
 
 // Custom Confirm Dialog
 const ui = {
-    confirm: async (message) => createConfirm(message)
-}
+  confirm: async (message) => createConfirm(message),
+};
 
 const createConfirm = (message) => {
-    return new Promise((complete, failed)=>{
-        $('#confirmMessage').text(message)
-        $('#confirmYes').off('click');
-        $('#confirmNo').off('click');
-        $('#confirmYes').on('click', ()=> { $('.confirm').hide(); complete(true); });
-        $('#confirmNo').on('click', ()=> { $('.confirm').hide(); complete(false); });
-        $('.confirm').show();
+  return new Promise((complete, failed) => {
+    $("#confirmMessage").text(message);
+    $("#confirmYes").off("click");
+    $("#confirmNo").off("click");
+    $("#confirmYes").on("click", () => {
+      $(".confirm").hide();
+      complete(true);
     });
-}
-                    
-const showConfirmDialog = async (questionStr, allowCallback, callback, callbackParam) => {
-    const confirm = await ui.confirm(questionStr);
-    if(confirm) {
-        if(allowCallback && callbackParam) {
-            callback(callbackParam);
-        }
-        else if(allowCallback && !callbackParam) {
-            callback();
-        }
+    $("#confirmNo").on("click", () => {
+      $(".confirm").hide();
+      complete(false);
+    });
+    $(".confirm").show();
+  });
+};
+
+const showConfirmDialog = async (
+  questionStr,
+  allowCallback,
+  callback,
+  callbackParam
+) => {
+  const confirm = await ui.confirm(questionStr);
+  if (confirm) {
+    if (allowCallback && callbackParam) {
+      callback(callbackParam);
+    } else if (allowCallback && !callbackParam) {
+      callback();
     }
-}
+  }
+};
 
 function addressRemoval(addressId) {
+  showToast("Address deleted successfully");
 
-    showToast('Address deleted successfully');
-    
-    // Shopify.bind(showToast);
-    setTimeout(() => { 
-        Shopify.postLink("/account/addresses/" + addressId + "?deleted=y", {
-            parameters: {
-                _method: "delete"
-            }
-        });
-    }, 4000);
+  // Shopify.bind(showToast);
+  setTimeout(() => {
+    Shopify.postLink("/account/addresses/" + addressId + "?deleted=y", {
+      parameters: {
+        _method: "delete",
+      },
+    });
+  }, 4000);
 
-    return false;
+  return false;
 }
 
 // Toast/Snackbar
 function showToast(message) {
+  setTimeout(() => {
+    let defaultMessage = "Updated";
+    // Get the snackbar DIV/Element
+    let toastEl = document.getElementById("snackbar");
 
-    setTimeout( () => {
-        let defaultMessage = 'Updated';
-        // Get the snackbar DIV/Element
-        let toastEl = document.getElementById("snackbar");
+    // Add the "show" class to DIV/Element
+    toastEl.className = "show";
+    message
+      ? (toastEl.innerHTML = message)
+      : (toastEl.innerHTML = defaultMessage);
 
-        // Add the "show" class to DIV/Element
-        toastEl.className = "show";
-        message ? toastEl.innerHTML = message : toastEl.innerHTML = defaultMessage;
-
-        // After 5 seconds, remove 'show' class from DIV
-        setTimeout(() => { toastEl.className = toastEl.className.replace("show", ""); }, 5000);
-    }, 3000);
+    // After 5 seconds, remove 'show' class from DIV
+    setTimeout(() => {
+      toastEl.className = toastEl.className.replace("show", "");
+    }, 5000);
+  }, 3000);
 }
 /* Continue developing this method if we have to display schedules for each store in Our Stores page.sca-storelocator.liquid. If this functionality not needed remove it. */
 function showLocatorInfo(e) {
-    let target = e.currentTarget;
-    let $el = $(e.currentTarget);
+  let target = e.currentTarget;
+  let $el = $(e.currentTarget);
 
-    let addressObj = {
-        title: $el.find("#scasl-title").html(),
-        address: $el.find("#scasl-address").html(),
-        phone: $el.find("#scasl-phone").find('a').html(),
-        schedule: $el.find("scasl-schedule").html(),
-    }
+  let addressObj = {
+    title: $el.find("#scasl-title").html(),
+    address: $el.find("#scasl-address").html(),
+    phone: $el.find("#scasl-phone").find("a").html(),
+    schedule: $el.find("scasl-schedule").html(),
+  };
 }
